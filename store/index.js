386c9e637718bc4713549507f3c95083a8134bf5
Piyush Mehta <me@piyushmehta.com>
2019-09-01 02:53:18 +0530

    async await

--- store/index.js
+++ store/index.js
@@ -1,67 +1,66 @@
-import {database} from '~/plugins/firebase'
-import axios from 'axios';
- 
-
+import { database } from "~/plugins/firebase";
+import axios from "axios";
 
 export const state = () => ({
   drawer: true,
-  content : "hi"
-})
+  content: "hi"
+});
 
 export const mutations = {
   toggleDrawer(state) {
-    state.drawer = !state.drawer
+    state.drawer = !state.drawer;
   },
   drawer(state, val) {
-    state.drawer = val
+    state.drawer = val;
   },
-  setcntent(state,data){
-    state.content = data
+  setcntent(state, data) {
+    state.content = data;
   },
-  savecontent(state,data){
-    state.content = data
-    
-    database.ref("text").child("newdoc").update({ content:state.content })
+  savecontent(state, data) {
+    state.content = data;
+
+    database
+      .ref("text")
+      .child("newdoc")
+      .update({ content: state.content });
   },
 
-  postdata(state,data){
-    axios.post(
-      "https://674ce112.ngrok.io/",
-      {
+  async postdata(state, data) {
+    await axios
+      .post("https://33f358c2.ngrok.io/", {
         age: data.age,
         family_history: data.family_history,
         leave: data.leave,
-        work_interfere:data.work_interfere,
-        benefits:data.benefits,
+        work_interfere: data.work_interfere,
+        benefits: data.benefits,
         anonymity: data.anonymity,
         phys_health_consequence: data.phys_health_consequence,
         mental_health_interview: data.mental_health_interview,
         obs_consequence: data.obs_consequence,
-        phone:data.phone,
-        remote_work:data.remote_work
+        phone: data.phone,
+        remote_work: data.remote_work
+      })
+      .then(function(res) {
+        console.log(res);
+      });
+  }
+};
+
+export const actions = {
+  async getcontent({ commit }) {
+    const dbref = database.ref("text").child("newdoc");
+    await dbref.on(
+      "value",
+      function(snapshot) {
+        const data = snapshot.val();
+        //this.datai = data;
+        //this.getdata(data);
+        console.log(data);
+        commit("setcntent", data.content);
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
       }
-      
-    ).then(function(res){
-      console.log(res)
-    })
+    );
   }
-}
-
-export const actions ={
-
-
-
-  async getcontent({commit}){
-    const dbref = database.ref("text").child("newdoc")
-    await dbref.on("value", function (snapshot) {
-      const data = snapshot.val()
-      //this.datai = data;
-      //this.getdata(data);
-      console.log(data)
-      commit("setcntent",data.content)
-
-    }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
-    });
-  }
-}
+};
